7edae90ae4119aa49935367f06905090f9217744
juliopomba <31495262+juliopomba@users.noreply.github.com>
2026-04-13 15:54:50 -0300

    Update config.js

--- civs-fix/src/config.js
+++ civs-fix/src/config.js
@@ -9,8 +9,8 @@ export const CONFIG = {
   ano:            "2024",
 
   // Logo: "shield" | "cross" | "heart" | "star" | "custom"
-  logoTipo:       "shield",
-  logoImagemUrl:  "",          // URL da imagem se logoTipo = "custom"
+  logoTipo:       "custom",
+  logoImagemUrl:  "https://media.canva.com/v2/image-resize/format:PNG/height:572/quality:100/uri:ifs%3A%2F%2FM%2F5a56df8e-5f90-4eb1-9e7d-fe24d0c7e390/watermark:F/width:1024?csig=AAAAAAAAAAAAAAAAAAAAABONhLaCGv_ZMwIUAb5eMC3o6cvhHXCsyNbo62lS7twa&exp=1776123439&osig=AAAAAAAAAAAAAAAAAAAAAIYDsbH332RLVTgeq4h8sxS5Z4yXEbVks0bN_AmIwZnp&signer=media-rpc&x-canva-quality=screen_3x",          // URL da imagem se logoTipo = "custom"
 
   corPrimaria:       "#0A1F44",
   corDestaque:       "#F2C94C",
