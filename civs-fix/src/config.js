65b6e3357a63e459a1869df7d17cb2aed14817db
juliopomba <31495262+juliopomba@users.noreply.github.com>
2026-04-13 16:04:50 -0300

    Update config.js

--- civs-fix/src/config.js
+++ civs-fix/src/config.js
@@ -10,7 +10,7 @@ export const CONFIG = {
 
   // Logo: "shield" | "cross" | "heart" | "star" | "custom"
   logoTipo:       "custom",
-  logoImagemUrl:  "https://media.canva.com/v2/image-resize/format:PNG/height:572/quality:100/uri:ifs%3A%2F%2FM%2F5a56df8e-5f90-4eb1-9e7d-fe24d0c7e390/watermark:F/width:1024?csig=AAAAAAAAAAAAAAAAAAAAABONhLaCGv_ZMwIUAb5eMC3o6cvhHXCsyNbo62lS7twa&exp=1776123439&osig=AAAAAAAAAAAAAAAAAAAAAIYDsbH332RLVTgeq4h8sxS5Z4yXEbVks0bN_AmIwZnp&signer=media-rpc&x-canva-quality=screen_3x",          // URL da imagem se logoTipo = "custom"
+  logoImagemUrl:  "https://chatgpt.com/backend-api/estuary/content?id=file_000000005b98720e9e55a576633320a4&ts=493363&p=fs&cid=1&sig=f04faa1db4730809ee48412b9806857db5fbf9bf2b68b36ca020e0069278b516&v=0",          // URL da imagem se logoTipo = "custom"
 
   corPrimaria:       "#0A1F44",
   corDestaque:       "#F2C94C",
